569a83ca8b7b1f24321f80fefd70db524db3f2d9
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-10-17 19:16:10 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Exports data in the CSV format"""
+"""
+Using https://jsonplaceholder.typicode.com REST API with a given imployee ID
+Records all tasks that are owned by this employee in CSV file.
+"""
+import csv
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-
-    import csv
-    import requests
-    import sys
-
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    name = user.json().get('username')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-
-    filename = userId + '.csv'
-    with open(filename, mode='w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL, lineterminator='\n')
-        for task in todos.json():
-            if task.get('userId') == int(userId):
-                writer.writerow([userId, name, str(task.get('completed')),
+    userID = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userID)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(userID)).json()
+    with open("{}.csv".format(userID), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            taskwriter.writerow([int(userID),
+                                 user.get('username'),
+                                 task.get('completed'),
                                  task.get('title')])
